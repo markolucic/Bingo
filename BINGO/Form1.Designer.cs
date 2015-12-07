8a16fe3f23f8ada3a545d49a0bc66c71a2f488d0
Marko <marko_lucic_1994@yahoo.com>
2015-12-07 23:31:49 +0100

    first commit

---
+++ BINGO/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace BINGO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_kuglica = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_dobitan = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_br_pogodjenih = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_rb_izvl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_kuglica
+            // 
+            this.btn_kuglica.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_kuglica.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_kuglica.Location = new System.Drawing.Point(276, 12);
+            this.btn_kuglica.Name = "btn_kuglica";
+            this.btn_kuglica.Size = new System.Drawing.Size(185, 173);
+            this.btn_kuglica.TabIndex = 9;
+            this.btn_kuglica.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 234);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(30, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.validacija);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(193, 173);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Unesite brojeve [1, 49]:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 234);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.validacija);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 234);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.validacija);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(170, 234);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(30, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.validacija);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(223, 234);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(30, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.validacija);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(276, 234);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(30, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextChanged += new System.EventHandler(this.validacija);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(329, 234);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(30, 20);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.TextChanged += new System.EventHandler(this.validacija);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(386, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_kuglica_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lbl_dobitan
+            // 
+            this.lbl_dobitan.AutoSize = true;
+            this.lbl_dobitan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dobitan.ForeColor = System.Drawing.Color.Red;
+            this.lbl_dobitan.Location = new System.Drawing.Point(10, 330);
+            this.lbl_dobitan.Name = "lbl_dobitan";
+            this.lbl_dobitan.Size = new System.Drawing.Size(0, 25);
+            this.lbl_dobitan.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 18);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Broj pogodjenih:";
+            // 
+            // lbl_br_pogodjenih
+            // 
+            this.lbl_br_pogodjenih.AutoSize = true;
+            this.lbl_br_pogodjenih.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_br_pogodjenih.Location = new System.Drawing.Point(134, 276);
+            this.lbl_br_pogodjenih.Name = "lbl_br_pogodjenih";
+            this.lbl_br_pogodjenih.Size = new System.Drawing.Size(17, 18);
+            this.lbl_br_pogodjenih.TabIndex = 13;
+            this.lbl_br_pogodjenih.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 18);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Redni broj izvlacenja:";
+            // 
+            // lbl_rb_izvl
+            // 
+            this.lbl_rb_izvl.AutoSize = true;
+            this.lbl_rb_izvl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_rb_izvl.Location = new System.Drawing.Point(167, 303);
+            this.lbl_rb_izvl.Name = "lbl_rb_izvl";
+            this.lbl_rb_izvl.Size = new System.Drawing.Size(16, 18);
+            this.lbl_rb_izvl.TabIndex = 15;
+            this.lbl_rb_izvl.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 377);
+            this.Controls.Add(this.lbl_rb_izvl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_br_pogodjenih);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_dobitan);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_kuglica);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BINGO by Marko Lucic";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_kuglica;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_dobitan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_br_pogodjenih;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_rb_izvl;
+    }
+}
+
---
+++ BINGO/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BINGO
+{
+    public partial class Form1 : Form
+    {
+        int ukupno_kugli = 48;
+        int[] kuglice = new int[35];
+        //int[] kuglice = { 1, 2, 3, 4, 5, 6, 7 };
+        int izvucenih = 0;
+        int br_pogodjenih = 0;
+        TextBox[] tb_moji_brojevi = new TextBox[6];
+        int dobitak = 0;
+        bool dobitnik = false;
+        int brojac_kugli = 0;
+
+        int[] plave = { 3, 10, 17, 24, 31, 38, 45 };
+        int[] zute = { 2, 9, 16, 23, 30, 37, 44 };
+        int[] narandzaste = { 4, 11, 18, 25, 32, 39, 46 };
+        int[] zelene = { 5, 12, 19, 26, 33, 40, 47 };
+        int[] roze = { 6, 13, 20, 27, 34, 41, 48 };
+        int[] ljubicaste = { 7, 14, 21, 28, 35, 42, 49 };
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void btn_kuglica_Click(object sender, EventArgs e)
+        {
+            reset();
+            if (checkMyNumbers())
+            {
+                izvuciKugle();
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Morate unijeti svih 7 brojeva!");
+            }
+
+        }
+
+        
+        void timer_Tick(object sender, EventArgs e)
+        {
+            /*
+            izvucenih++;
+            lbl_rb_izvl.Text = izvucenih.ToString();
+            int kugla = izvuciKuglu();
+            
+            btn_kuglica.ForeColor = Color.Black;
+            if (izvucena(kugla))
+            {
+                br_pogodjenih++;
+                lbl_br_pogodjenih.Text = br_pogodjenih.ToString();
+                btn_kuglica.ForeColor = Color.Red;
+            }
+            if (br_pogodjenih == 7)
+            {
+                dobitak = izvucenih;
+                dobitnik = true;
+            }
+            btn_kuglica.Text = kugla.ToString();
+            richTextBox1.Text += kugla + ", ";
+            if (izvucenih == 34)
+            {
+                timer1.Stop();
+                if (dobitnik == true)
+                {
+                    int KM = pogodjeno(dobitak);
+                    lbl_dobitan.Text = "Listic je DOBITAN (" + KM + "KM)";
+                }
+                MessageBox.Show("Izvlacenje je zavrseno.");
+            }
+            */
+            izvucenih++;
+            btn_kuglica.ForeColor = Color.Black;
+            btn_kuglica.Text = kuglice[izvucenih-1].ToString();
+            btn_kuglica.BackgroundImage = bojaKugle(btn_kuglica);
+            richTextBox1.Text += kuglice[izvucenih-1] + ", ";
+            lbl_rb_izvl.Text = (izvucenih).ToString();
+            if (izvucena(kuglice[izvucenih-1]))
+            {
+                br_pogodjenih++;
+                lbl_br_pogodjenih.Text = br_pogodjenih.ToString();
+                btn_kuglica.ForeColor = Color.Red;
+            }
+            if (br_pogodjenih == 7 && dobitnik == false)
+            {
+                dobitak = izvucenih; 
+                dobitnik = true;
+            }
+            if (izvucenih == 35)
+            {
+                timer1.Stop();
+                if (dobitnik == true)
+                {
+                    int KM = pogodjeno(dobitak);
+                    lbl_dobitan.Text = "Listic je DOBITAN (" + KM + "KM)";
+                }
+                else
+                {
+                    lbl_dobitan.Text = "Listic nije DOBITAN";
+                    MessageBox.Show("Izvlacenje je zavrseno.");
+                }
+            }
+            
+        }
+
+        public Image bojaKugle(Button b)
+        {
+            if(plave.Contains(int.Parse(b.Text)))
+            {
+                return Properties.Resources.plava;
+            }
+            if (zute.Contains(int.Parse(b.Text)))
+            {
+                return Properties.Resources.zuta;
+            }
+            if (narandzaste.Contains(int.Parse(b.Text)))
+            {
+                return Properties.Resources.narandzasta;
+            }
+            if (ljubicaste.Contains(int.Parse(b.Text)))
+            {
+                return Properties.Resources.ljubicasta;
+            }
+            if (zelene.Contains(int.Parse(b.Text)))
+            {
+                return Properties.Resources.zelena;
+            }
+            if (roze.Contains(int.Parse(b.Text)))
+            {
+                return Properties.Resources.roza;
+            }
+            return Properties.Resources.plava;
+        }
+
+        public bool checkMyNumbers()
+        {
+            if(textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "" && textBox7.Text != "" ) 
+                return true; // ne smije se unijeti isti broj, mora biti u opsegu 1..49
+            return false;
+        }
+        
+        //public int izvuciKuglu()
+        //{
+            //int broj, broj2;
+            //Random r = new Random();
+            //broj = r.Next(49) + 1;
+            //if (kuglice.Contains(broj))
+            //{
+            //    broj2 = r.Next(49) + 1;
+            //    kuglice[brojac_kugli++] = broj2;
+            //    return broj2;
+            //} 
+            //kuglice[brojac_kugli++] = broj;
+            //return broj;   
+        //}
+
+        public void izvuciKugle()
+        {
+            var rnd = new Random();
+            var randomNumbers = Enumerable.Range(1, 49).OrderBy(x => rnd.Next()).Take(35).ToList();
+            
+            for (int i = 0; i < 35; i++)
+            {
+                kuglice[i] = randomNumbers[i];
+            }
+        }
+
+        public bool izvucena(int kugla)
+        {
+            if (textBox1.Text == kugla.ToString() || textBox2.Text == kugla.ToString() || textBox3.Text == kugla.ToString() || textBox4.Text == kugla.ToString() || textBox5.Text == kugla.ToString() || textBox6.Text == kugla.ToString() || textBox7.Text == kugla.ToString())
+                return true;
+            return false;
+        }
+        
+        public void reset()
+        {
+            richTextBox1.Text = "";
+            izvucenih = 0;
+            br_pogodjenih = 0;
+            dobitak = 0;
+            dobitnik = false;
+            lbl_br_pogodjenih.Text = "0";
+            lbl_rb_izvl.Text = "";
+            lbl_dobitan.Text = "";
+            //textBox1.Text = "";
+            //textBox2.Text = "";
+            //textBox3.Text = "";
+            //textBox4.Text = "";
+            //textBox5.Text = "";
+            //textBox6.Text = "";
+            //textBox7.Text = "";
+            btn_kuglica.Text = "";
+        }
+        public int pogodjeno(int dobitak)
+        {
+            switch (dobitak)
+            {
+                case 7: return 10000;
+                case 8: return 7500;
+                case 9: return 5000;
+                case 10: return 2500;
+                case 11: return 1000;
+                case 12: return 500;
+                case 13: return 300;
+                case 14: return 200;
+                case 15: return 100;
+                case 16: return 90;
+                case 17: return 80;
+                case 18: return 70;
+                case 19: return 60;
+                case 20: return 50;
+                case 21: return 40;
+                case 22: return 30;
+                case 23: return 25;
+                case 24: return 20;
+                case 25: return 15;
+                case 26: return 10;
+                case 27: return 9;
+                case 28: return 8;
+                case 29: return 7;
+                case 30: return 6;
+                case 31: return 5;
+                case 32: return 4;
+                case 33: return 3;
+                case 34: return 2;
+                case 35: return 1;
+                default:
+                    return 0;
+            }
+        }
+
+        private void validacija(object sender, EventArgs e)
+        {
+            int brojKuglice;
+            TextBox t = (TextBox) sender;
+            if (!int.TryParse(t.Text, out brojKuglice))
+            {
+                //MessageBox.Show("Greska prilikom unosa!");
+                t.Text = "";
+            }
+            TextBox[] kugle = { textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, textBox7 };
+            
+        }
+    }
+}
