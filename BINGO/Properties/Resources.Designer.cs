9819be6ba6ca8609f2350f800ad0b98feab4d40d
Marko <marko_lucic_1994@yahoo.com>
2015-12-07 23:41:45 +0100

    ubacene nove kugle

--- BINGO/Properties/Resources.Designer.cs
+++ BINGO/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace BINGO.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap kuglica {
-            get {
-                object obj = ResourceManager.GetObject("kuglica", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap ljubicasta {
             get {
                 object obj = ResourceManager.GetObject("ljubicasta", resourceCulture);
